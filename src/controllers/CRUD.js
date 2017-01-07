30627794ff340a67a3255f3b643898b7e8747533
Sindre Svendby <sinsvend@gmail.com>
2017-01-07 12:03:35 +0100

    remove unknown function

--- src/controllers/CRUD.js
+++ src/controllers/CRUD.js
@@ -185,22 +185,6 @@ class CRUD {
 
 export default CRUD;
 
-function censor(censor) {
-  var i = 0;
-
-  return function(key, value) {
-    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value)
-      return '[Circular]';
-
-    if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-      return '[Unknown]';
-
-    ++i; // so we know we aren't using the original object anymore
-
-    return value;
-  }
-}
-
 function createOr(describeData, query, key) {
   const orKeys = key.split('|');
   const orQuery = orKeys.map(key => createFieldStatement(describeData, query, key))
